f1b284eaae01f10eafc678d30784a147be717559
Math3uz7 <matheuspprudencio@gmail.com>
2025-04-01 22:31:08 -0300

    final

--- cadastro/assets/js/script.js
+++ cadastro/assets/js/script.js
@@ -1,125 +1,214 @@
-
 function take() {
-    
-    let pegaNome0= document.querySelector("#nome");
-    let resultadoNome = pegaNome0.value;
-    localStorage.setItem("nome", resultadoNome);
+  let pegaNome0 = document.querySelector("#nome");
+  let resultadoNome = pegaNome0 ? pegaNome0.value : "";
+  localStorage.setItem("nome", resultadoNome);
 
-    // _________________________________________________________________
+  let pegaData = document.querySelector("#date");
+  let resultadoData = pegaData ? pegaData.value : "";
+  localStorage.setItem("data de nascimento", resultadoData);
 
-    let pegaData = document.querySelector("#date");
-    let resultadoData = pegaData.value;
-    localStorage.setItem("data de nascimento", resultadoData);
-    
-    // _________________________________________________________________
-    
-    let pegaEmail = document.querySelector("#email");
-    let resultadoEmail = pegaEmail.value;
-    localStorage.setItem("Email", resultadoEmail);
+  let pegaEmail = document.querySelector("#email");
+  let resultadoEmail = pegaEmail ? pegaEmail.value : "";
+  localStorage.setItem("Email", resultadoEmail);
 
-    // _________________________________________________________________
+  let pegaSenha = document.querySelector("#senha");
+  let resultadoSenha = pegaSenha ? pegaSenha.value : "";
+  localStorage.setItem("senha", resultadoSenha);
 
-    let pegaSenha = document.querySelector("#senha");
-    let resultadoSenha = pegaSenha.value;
-    localStorage.setItem("senha", resultadoSenha);
-    
-    // _________________________________________________________________
-    
-    let pegaCSenha = document.querySelector("#confSenha");
-    let resultadoCSenha = pegaCSenha.value;
-    localStorage.setItem("senhaConfirma", resultadoCSenha);
-    
-    // _________________________________________________________________
-    
-    let pegaCEP = document.querySelector("#CEP");
-    let resultadoCEP = pegaCEP.value;
-    localStorage.setItem("CEP", resultadoCEP);
+  let pegaCSenha = document.querySelector("#confSenha");
+  let resultadoCSenha = pegaCSenha ? pegaCSenha.value : "";
+  localStorage.setItem("senhaConfirma", resultadoCSenha);
 
-    // __________________________________________________________________
+  let pegaCEP = document.querySelector("#CEP");
+  let resultadoCEP = pegaCEP ? pegaCEP.value : "";
+  localStorage.setItem("CEP", resultadoCEP);
 
-    let pegarua = document.querySelector("#rua");
-    let resultadorua = pegarua.value;
-    localStorage.setItem("Rua", resultadorua);
+  let pegarua = document.querySelector("#rua");
+  let resultadorua = pegarua ? pegarua.value : "";
+  localStorage.setItem("Rua", resultadorua);
 
-    // __________________________________________________________________
+  let pegacidade = document.querySelector("#cidade");
+  let resultadocidade = pegacidade ? pegacidade.value : "";
+  localStorage.setItem("Cidade", resultadocidade);
 
-    let pegacidade = document.querySelector("#cidade");
-    let resultadocidade = pegacidade.value;
-    localStorage.setItem("Cidade", resultadocidade);
+  let pegaestado = document.querySelector("#estado");
+  let resultadoestado = pegaestado ? pegaestado.value : "";
+  localStorage.setItem("Estado", resultadoestado);
 
-    // __________________________________________________________________
-
-    let pegaestado = document.querySelector("#estado");
-    let resultadoestado = pegaestado.value;
-    localStorage.setItem("Estado", resultadoestado);
-     // _________________________________________________________________
-
-     let pegasexo = document.querySelector("#sexo");
-     let resultadosexo = pegasexo.value;
-     localStorage.setItem("Sexo", resultadosexo);
-    // __________________________________________________________________
+  let pegasexo = document.querySelector("#sexo");
+  let resultadosexo = pegasexo ? pegasexo.value : "";
+  localStorage.setItem("Sexo", resultadosexo);
 
 
-    let pegaNome = document.querySelector("#nome");
-    pegaNome.value;
-    if (resultadoSenha == resultadoCSenha) {
-        let pegar = document.querySelector("#click");
-        pegar.style.display = "block";
-
-    }
-    else{
-        alert("Suas senhas são diferentes");
-       localStorage.removeItem("senhaConfirma");
-       localStorage.removeItem("senha");
-    }
-
-    if(resultadoNome == "" || resultadoData == "" || resultadoEmail == "" || resultadoCSenha == "" || resultadoSenha == ""){
+  // Verificação de campos vazios antes de validar senhas
+  if (resultadoNome === "" || resultadoData === "" || resultadoEmail === "" || resultadoCSenha === "" || resultadoSenha === "") {
       alert("Preencha todos os campos");
-    }
-    else{
-      window.location ="../login/login.html";
-
-    }
-    
-    let valida = localStorage.getItem("bi");
-    valida.localStorage.setitem("bi",  "");
+      let link = document.getElementById("link");
+      link.href = "./cadastro.html"
+      return;
+  }
 
 
-    let guardaNome = localStorage.getItem("nome", resultadoNome);
-    let guardaData = localStorage.getItem("date", resultadoData);
-    let guardaEmail = localStorage.getItem("email", resultadoEmail);
-    let guardaSenha = localStorage.getItem("senha", resultadoSenha);
-    let guardaCSenha = localStorage.getItem("confSenha", resultadoCSenha);
+  // Verificação de senha antes de exibir o botão
+  if (resultadoSenha === resultadoCSenha) {
+      let pegar = document.querySelector("#click");
+      if (pegar) {
+          pegar.style.display = "block";
+          alert("Cadastro Concluido com Sucesso !")
+          let link = document.getElementById("link");
+          link.href = "../login/login.html"
+      }
+  } else {
+      alert("Suas senhas são diferentes");
 
-     if(resultadoNome == guardaNome || resultadoData == guardaData || resultadoEmail == guardaEmail || resultadoCSenha == guardaCSenha || resultadoSenha == guardaSenha){
-      alert("Cadastro Concluido");
-      valida.localStorage.setitem("bi", 1);
-      window.location ="../index.html";
+      let link = document.getElementById("link");
+      link.href = "./cadastro.html"
+
+      localStorage.removeItem("senha");
+      localStorage.removeItem("senhaConfirma");
+      localStorage.removeItem("Estado");
+      localStorage.removeItem("CEP");      
+      localStorage.removeItem("Rua");
+      localStorage.removeItem("Cidade");
+      localStorage.removeItem("Sexo");
+      localStorage.removeItem("nome");      
+      localStorage.removeItem("data de nascimento");
+      localStorage.removeItem("Email");      
     }
-    
-     if (valida == 1) {
-          
-      let perfil2 =  document.querySelector(".perfil").style.display = "block";
-      let local2 = document.getElementById("join").style.display = "none";
-      
-      localStorage.setItem("entre" , perfil2);
-      localStorage.setItem("perfil", local2);
-    
-      
-    }
-    else if(valida == ""){
-      
-      let perfil2 =  document.querySelector(".perfil").style.display = "none";  
-      let local2 = document.getElementById("join").style.display = "block";
-    
-    
-      localStorage.setItem("entre" , perfil2);
-      localStorage.setItem("perfil", local2);
-    
-    
-     }
-     
+
+    link.setAttribute("href", "../login/login.html");
+
 }
+
+
+
+// function take() {
+    
+//     let pegaNome0= document.querySelector("#nome");
+//     let resultadoNome = pegaNome0.value;
+//     localStorage.setItem("nome", resultadoNome);
+
+//     // _________________________________________________________________
+
+//     let pegaData = document.querySelector("#date");
+//     let resultadoData = pegaData.value;
+//     localStorage.setItem("data de nascimento", resultadoData);
+    
+//     // _________________________________________________________________
+    
+//     let pegaEmail = document.querySelector("#email");
+//     let resultadoEmail = pegaEmail.value;
+//     localStorage.setItem("Email", resultadoEmail);
+
+//     // _________________________________________________________________
+
+//     let pegaSenha = document.querySelector("#senha");
+//     let resultadoSenha = pegaSenha.value;
+//     localStorage.setItem("senha", resultadoSenha);
+    
+//     // _________________________________________________________________
+    
+//     let pegaCSenha = document.querySelector("#confSenha");
+//     let resultadoCSenha = pegaCSenha.value;
+//     localStorage.setItem("senhaConfirma", resultadoCSenha);
+    
+//     // _________________________________________________________________
+    
+//     let pegaCEP = document.querySelector("#CEP");
+//     let resultadoCEP = pegaCEP.value;
+//     localStorage.setItem("CEP", resultadoCEP);
+
+//     // __________________________________________________________________
+
+//     let pegarua = document.querySelector("#rua");
+//     let resultadorua = pegarua.value;
+//     localStorage.setItem("Rua", resultadorua);
+
+//     // __________________________________________________________________
+    
+//     let pegacidade = document.querySelector("#cidade");
+//     let resultadocidade = pegacidade.value;
+//     localStorage.setItem("Cidade", resultadocidade);
+    
+//     // __________________________________________________________________
+    
+//     let pegaestado = document.querySelector("#estado");
+//     let resultadoestado = pegaestado.value;
+//     localStorage.setItem("Estado", resultadoestado);
+//     // _________________________________________________________________
+
+//     let pegasexo = document.querySelector("#sexo");
+//     let resultadosexo = pegasexo.value;
+//     localStorage.setItem("Sexo", resultadosexo);
+//     // __________________________________________________________________
+
+
+//     let pegaNome = document.querySelector("#nome");
+//     pegaNome.value;
+
+//     if (resultadoSenha == resultadoCSenha) {
+//         let pegar = document.querySelector("#click");
+//         pegar.style.display = "block";
+
+//     }
+//     else{
+//        alert("Suas senhas são diferentes");
+//        localStorage.removeItem(resultadoSenha);
+//        localStorage.removeItem(resultadoCSenha);
+//     }
+
+
+
+
+//     if(resultadoNome == "" || resultadoData == "" || resultadoEmail == "" || resultadoCSenha == "" || resultadoSenha == ""){
+//       alert("Preencha todos os campos");
+//     }
+
+// ___________________________________________________________________________________ DIV
+    
+
+//     let guardaNome = localStorage.getItem("nome", resultadoNome);
+//     let guardaData = localStorage.getItem("date", resultadoData);
+//     let guardaEmail = localStorage.getItem("email", resultadoEmail);
+//     let guardaSenha = localStorage.getItem("senha", resultadoSenha);
+//     let guardaCSenha = localStorage.getItem("confSenha", resultadoCSenha);
+    
+    
+//     if(resultadoNome == guardaNome || resultadoData == guardaData || resultadoEmail == guardaEmail || resultadoCSenha == guardaCSenha || resultadoSenha == guardaSenha){
+      
+//       alert("Cadastro Concluido"); 
+//       valida.localStorage.setitem("bi", 1);
+  
+      
+//       let perfil2 =  document.querySelector(".perfil").style.display = "block";
+//       let local2 = document.getElementById("join").style.display = "none";
+      
+//       localStorage.setItem("entre" , perfil2);
+//       localStorage.setItem("perfil", local2);
+  
+//       window.location = ".../login/login.html";
+  
+// }
+
+    
+    //  if (valida == 1) {
+          
+    
+      
+    // }
+    // else if(valida == null){
+      
+    //   let perfil2 =  document.querySelector(".perfil").style.display = "none";  
+    //   let local2 = document.getElementById("join").style.display = "block";
+    
+    
+    //   localStorage.setItem("entre" , perfil2);
+    //   localStorage.setItem("perfil", local2);
+    
+    
+    //  }
+     
+// }
 
 
 // ______________________________________________________
--- login/assets/js/script.js
+++ login/assets/js/script.js
@@ -1,87 +1,145 @@
-function entrar() {
-  let binario;
+// function entrar() {
 
-    let email = document.querySelector("#meuEmail")
-    let valoremail = email.value
+//     let email = document.querySelector("#meuEmail")
+//     let valoremail = email.value
 
-    let senha = document.querySelector("#minhaSenha")
-    let valorsenha = senha.value
+//     let senha = document.querySelector("#minhaSenha")
+//     let valorsenha = senha.value
 
-    let getEmail = localStorage.getItem("Email")
-    let getSenha = localStorage.getItem("senha")
-    
-    
-    
-    if (valoremail == getEmail || valorsenha == getSenha  ) {
-      alert("Cadastro Concluido")
-      window.location.href =".../index.html"
+//     let getEmail = localStorage.getItem("Email")
+//     let getSenha = localStorage.getItem("senha")
+
+
+
+//  if (getEmail == valoremail && getSenha == valorsenha) {
       
-  }
+
+//   let perfil2 =  document.querySelector(".perfil").style.display = "block"
+//   let local2 = document.getElementById("join").style.display = "none"
   
-  
-  else {
-        alert("Email ou senha estão errados")
-    }
-
-    
-
-
-let valida = localStorage.getItem("bi")
-
-
- if (valida == 1) {
-      
-  let perfil2 =  document.querySelector(".perfil").style.display = "block"
-  let local2 = document.getElementById("join").style.display = "none"
-  
-  localStorage.setItem("entre" , perfil2)
-  localStorage.setItem("perfil", local2)
-
-
-}
-else if(valida == ""){
-  
-  let perfil2 =  document.querySelector(".perfil").style.display = "none"    
-  let local2 = document.getElementById("join").style.display = "block"
-
-
-  localStorage.setItem("entre" , perfil2)
-  localStorage.setItem("perfil", local2)
-
-  window.location.href =".../login/login.html"
- }
- }
-
-
-
-
-// const changeTheme = document.getElementById("tema")
-
-// function blackTheme(){
-//   document.body.classList.toggle("white")
+//   localStorage.setItem("entre" , perfil2)
+//   localStorage.setItem("perfil", local2)
 // }
 
-// function loadTheme(){
-//   const blackMode = localStorage.getItem("white") 
 
-// if(blackMode){
-// blackTheme()
+//  if(getEmail != valoremail || getSenha != valorsenha || valoremail == null || valorsenha == null){
+//   alert("Falha ao entrar")
+  
+//   let perfil2 =  document.querySelector(".perfil").style.display = "none"    
+//   let local2 = document.getElementById("join").style.display = "block"
+
+
+//   localStorage.setItem("entre" , perfil2)
+//   localStorage.setItem("perfil", local2)
+
+//  }
+
+
+// }
+
+
+// function entrar() {
+//   let email = document.querySelector("#meuEmail");
+//   let senha = document.querySelector("#minhaSenha");
+
+//   let valoremail = email ? email.value.trim() : "";
+//   let valorsenha = senha ? senha.value.trim() : "";
+
+//   let getEmail = localStorage.getItem("Email");
+//   let getSenha = localStorage.getItem("senha");
+
+//   let perfil2 = document.querySelector(".perfil");
+//   let local2 = document.getElementById("join");
+
+//   // Verifica se o localStorage tem os dados cadastrados
+//   if (!getEmail || !getSenha) {
+//       alert("Nenhum usuário cadastrado! Faça seu cadastro primeiro.");
+//       return;
+//   }
+
+//   // Verifica se os campos foram preenchidos
+//   if (!valoremail || !valorsenha) {
+//       alert("Por favor, preencha todos os campos.");
+//       return;
+//   }
+
+//   // Validação do login
+//   if (getEmail === valoremail && getSenha === valorsenha) {
+//       alert("Login bem-sucedido!");
+
+//       if (perfil2) perfil2.style.display = "block";
+//       if (local2) local2.style.display = "none";
+
+//       localStorage.setItem("entre", "true"); // Indica que o usuário está logado
+//   } else {
+//       alert("Email ou senha incorretos!");
+
+//       if (perfil2) perfil2.style.display = "none";
+//       if (local2) local2.style.display = "block";
+
+//       localStorage.setItem("entre", "false"); // Indica falha no login
 //   }
 // }
 
-// loadTheme();
 
-// changeTheme.addEventListener("change",function(){
-//   blackTheme()
+function entrar() {
+  let email = document.querySelector("#meuEmail");
+  let senha = document.querySelector("#minhaSenha");
 
+  let valoremail = email ? email.value.trim() : "";
+  let valorsenha = senha ? senha.value.trim() : "";
 
-// localStorage.removeItem("white")
+  let getEmail = localStorage.getItem("Email");
+  let getSenha = localStorage.getItem("senha");
 
+  let perfil2 = document.querySelector(".perfil");
+  let local2 = document.getElementById("join");
 
-// if(document.body.classList.contains("white")){
-// localStorage.setItem("white",1)
-// }
-  
+  // Verifica se há usuário cadastrado no localStorage
+  if (!getEmail || !getSenha) {
+      alert("Nenhum usuário cadastrado! Faça seu cadastro primeiro.");
+      return;
+  }
 
-// })
- 
+  // Verifica se os campos foram preenchidos
+  if (!valoremail || !valorsenha) {
+      alert("Por favor, preencha todos os campos.");
+      return;
+  }
+
+  // Validação do login
+  if (getEmail === valoremail && getSenha === valorsenha) {
+      alert("Login bem-sucedido!");
+
+      if (perfil2) perfil2.style.display = "block";
+      if (local2) local2.style.display = "none";
+
+      localStorage.setItem("entre", "true"); // Indica que o usuário está logado permanentemente
+  } else {
+      alert("Email ou senha incorretos!");
+
+      if (perfil2) perfil2.style.display = "none";
+      if (local2) local2.style.display = "block";
+
+      localStorage.setItem("entre", "false"); // Indica falha no login
+  }
+}
+
+// Função para verificar o estado do login ao carregar a página
+function verificarLogin() {
+  let perfil2 = document.querySelector(".perfil");
+  let local2 = document.getElementById("join");
+
+  let estaLogado = localStorage.getItem("entre");
+
+  if (estaLogado === "true") {
+      if (perfil2) perfil2.style.display = "block";
+      if (local2) local2.style.display = "none";
+  } else {
+      if (perfil2) perfil2.style.display = "none";
+      if (local2) local2.style.display = "block";
+  }
+}
+
+// Verifica o login assim que a página é carregada
+document.addEventListener("DOMContentLoaded", verificarLogin);
