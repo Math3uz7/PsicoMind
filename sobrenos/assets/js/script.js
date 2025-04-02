625b114ecb5c0f4f39c732fac86d051bd1d4111c
Math3uz7 <matheuspprudencio@gmail.com>
2025-04-01 22:37:21 -0300

    final real

--- sobrenos/assets/js/script.js
+++ sobrenos/assets/js/script.js
@@ -1,37 +1,3 @@
-// const changeTheme = document.getElementById("tema")
-
-// function blackTheme(){
-//   document.body.classList.toggle("white")
-// }
-
-// function loadTheme(){
-//   const blackMode = localStorage.getItem("white") 
-
-// if(blackMode){
-// blackTheme()
-//   }
-// }
-
-// loadTheme();
-
-// changeTheme.addEventListener("change",function(){
-//   blackTheme()
-
-
-// localStorage.removeItem("white")
-
-
-// if(document.body.classList.contains("white")){
-// localStorage.setItem("white",1)
-// }
-  
-
-// })
- 
-
-// _________________________________________________________
-
-
 let valida = localStorage.getItem("bi")
 
 
